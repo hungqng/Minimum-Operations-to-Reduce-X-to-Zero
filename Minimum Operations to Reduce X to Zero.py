e0383335aecc1fe9657d5b9829eb24ec10920adc
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-11 00:21:19 -0700

    Add files via upload

--- Minimum Operations to Reduce X to Zero.py
+++ Minimum Operations to Reduce X to Zero.py
@@ -21,3 +21,17 @@ class Solution:
             return ans
         else:
             return len(nums)-ans
+
+        # Solution 2
+        cumsum = [0] + list(accumulate(nums))
+        dic = {c:i for i,c in enumerate(cumsum)}
+        goal = cumsum[-1] - x
+        ans = -float("inf")
+
+        if goal < 0: return -1
+
+        for num in dic:
+            if num + goal in dic:
+                ans = max(ans, dic[num + goal] - dic[num])
+
+        return len(nums) - ans if ans != -float("inf") else -1
